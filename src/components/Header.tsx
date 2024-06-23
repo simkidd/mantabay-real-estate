607823880ac4d3e8031f4520b7875cc94e2dcb64
John Mason <87030324+simkidd@users.noreply.github.com>
2024-06-23 16:16:14 +0100

    starter

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full'>
+      <div></div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      dmsans: ['"DM Sans", sans-serif'],
+      inter: ['"Inter", sans-serif'],
+      roboto: ['"Roboto", sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "#256252",
+          // 100: "",
+          // 200: "",
+          300: "#2E846D",
+        },
+        background: "transparent",
       },
+      backgroundImage: {},
     },
   },
   plugins: [],
